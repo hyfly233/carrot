6980fbed11c1662b029ea81089ddfbeffa7216c1
hyfly233 <hyfly233@outlook.com>
2021-07-04 12:00:00 +0800

    commit

--- internal/common/types.go
+++ internal/common/types.go
@@ -5,3 +5,9 @@ type Resource struct {
 	Memory int64 `json:"memory"` // MB
 	VCores int32 `json:"vcores"` // 虚拟核心数
 }
+
+// NodeID 节点标识
+type NodeID struct {
+	Host string `json:"host"`
+	Port int32  `json:"port"`
+}
