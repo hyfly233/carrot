fc2ed21c448c8f0c8e0df236e6189421fbaa3776
hyfly233 <hyfly233@outlook.com>
2021-07-05 12:00:00 +0800

    commit

--- internal/common/types.go
+++ internal/common/types.go
@@ -11,3 +11,9 @@ type NodeID struct {
 	Host string `json:"host"`
 	Port int32  `json:"port"`
 }
+
+// ApplicationID 应用程序标识
+type ApplicationID struct {
+	ClusterTimestamp int64 `json:"cluster_timestamp"`
+	ID               int32 `json:"id"`
+}
