7196b4e3321f21af044fd5b627463e033ae284b5
hyfly233 <hyfly233@outlook.com>
2021-10-06 12:00:00 +0800

    commit

--- cmd/nodemanager/main.go
+++ cmd/nodemanager/main.go
@@ -1,1 +1,36 @@
 package nodemanager
+
+import (
+	"carrot/internal/common"
+	"flag"
+	"log"
+)
+
+func main() {
+	var (
+		port   = flag.Int("port", 8042, "NodeManager port")
+		host   = flag.String("host", "localhost", "NodeManager host")
+		rmURL  = flag.String("rm-url", "http://localhost:8088", "ResourceManager URL")
+		memory = flag.Int64("memory", 8192, "Total memory in MB")
+		vcores = flag.Int("vcores", 8, "Total virtual cores")
+	)
+	flag.Parse()
+
+	log.Printf("Starting YARN NodeManager on %s:%d...", *host, *port)
+
+	nodeID := common.NodeID{
+		Host: *host,
+		Port: int32(*port),
+	}
+
+	totalResource := common.Resource{
+		Memory: *memory,
+		VCores: int32(*vcores),
+	}
+
+	nm := nodemanager.NewNodeManager(nodeID, totalResource, *rmURL)
+
+	if err := nm.Start(*port); err != nil {
+		log.Fatalf("Failed to start NodeManager: %v", err)
+	}
+}
