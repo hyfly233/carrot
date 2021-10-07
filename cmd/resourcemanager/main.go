c9fbde3672ecfa28fee119b1cc2120d0f3ad133e
hyfly233 <hyfly233@outlook.com>
2021-10-07 12:00:00 +0800

    commit

--- cmd/resourcemanager/main.go
+++ cmd/resourcemanager/main.go
@@ -1,1 +1,23 @@
 package resourcemanager
+
+import (
+	"flag"
+	"log"
+
+	"carrot/internal/resourcemanager"
+)
+
+func main() {
+	var (
+		port = flag.Int("port", 8088, "ResourceManager port")
+	)
+	flag.Parse()
+
+	log.Println("Starting YARN ResourceManager...")
+
+	rm := resourcemanager.NewResourceManager()
+
+	if err := rm.Start(*port); err != nil {
+		log.Fatalf("Failed to start ResourceManager: %v", err)
+	}
+}
