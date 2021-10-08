7bfdc5bccad64d4b040a0ea0a2c977345c8513d2
hyfly233 <hyfly233@outlook.com>
2021-10-08 12:00:00 +0800

    commit

--- internal/nodemanager/node_manager.go
+++ internal/nodemanager/node_manager.go
@@ -1,1 +1,21 @@
 package nodemanager
+
+import (
+	"carrot/internal/common"
+	"net/http"
+	"sync"
+	"time"
+)
+
+// NodeManager 节点管理器
+type NodeManager struct {
+	mu                 sync.RWMutex
+	nodeID             common.NodeID
+	resourceManagerURL string
+	totalResource      common.Resource
+	usedResource       common.Resource
+	containers         map[string]*Container
+	httpServer         *http.Server
+	heartbeatInterval  time.Duration
+	stopChan           chan struct{}
+}
