b99de7521312b5a1efd62a6c52aa1fa3493dbaa3
hyfly233 <hyfly233@outlook.com>
2021-11-12 12:00:00 +0800

    commit

--- internal/resourcemanager/scheduler/fifo/fifo_scheduler.go
+++ internal/resourcemanager/scheduler/fifo/fifo_scheduler.go
@@ -21,3 +21,18 @@ type ApplicationInfo struct {
 	ID       common.ApplicationID `json:"id"`
 	Resource common.Resource      `json:"resource"`
 }
+
+// FIFOScheduler 先进先出调度器
+type FIFOScheduler struct {
+	rm ResourceManagerInterface
+}
+
+// NewFIFOScheduler 创建 FIFO 调度器
+func NewFIFOScheduler() *FIFOScheduler {
+	return &FIFOScheduler{}
+}
+
+// SetResourceManager 设置资源管理器引用
+func (s *FIFOScheduler) SetResourceManager(rm ResourceManagerInterface) {
+	s.rm = rm
+}
