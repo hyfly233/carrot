2caeba69afd3fa163de22b53fa42e534c1e6215c
hyfly233 <hyfly233@outlook.com>
2021-11-13 12:00:00 +0800

    commit

--- internal/resourcemanager/scheduler/fifo/fifo_scheduler.go
+++ internal/resourcemanager/scheduler/fifo/fifo_scheduler.go
@@ -1,6 +1,10 @@
 package fifo
 
-import "carrot/internal/common"
+import (
+	"carrot/internal/common"
+	"fmt"
+	"log"
+)
 
 // ResourceManagerInterface 定义资源管理器接口，避免循环依赖
 type ResourceManagerInterface interface {
@@ -35,4 +39,38 @@ func NewFIFOScheduler() *FIFOScheduler {
 // SetResourceManager 设置资源管理器引用
 func (s *FIFOScheduler) SetResourceManager(rm ResourceManagerInterface) {
 	s.rm = rm
+}
+
+// Schedule 为应用程序调度容器
+func (s *FIFOScheduler) Schedule(app *ApplicationInfo) ([]*common.Container, error) {
+	if s.rm == nil {
+		return nil, fmt.Errorf("resource manager not set")
+	}
+
+	// 查找可用节点
+	node := s.findAvailableNode(app.Resource)
+	if node == nil {
+		return nil, fmt.Errorf("no available node for resource requirement: %+v", app.Resource)
+	}
+
+	// 创建容器
+	containerID := common.ContainerID{
+		ApplicationAttemptID: common.ApplicationAttemptID{
+			ApplicationID: app.ID,
+			AttemptID:     1,
+		},
+		ContainerID: 1,
+	}
+
+	container := &common.Container{
+		ID:       containerID,
+		NodeID:   node.ID,
+		Resource: app.Resource,
+		Status:   "ALLOCATED",
+		State:    common.ContainerStateNew,
+	}
+
+	log.Printf("Allocated container %v on node %s:%d", containerID, node.ID.Host, node.ID.Port)
+
+	return []*common.Container{container}, nil
 }
