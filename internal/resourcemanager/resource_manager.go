cc3f4225dc5a763a5cc852854124feef9a882c16
hyfly233 <hyfly233@outlook.com>
2021-11-18 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager.go
+++ internal/resourcemanager/resource_manager.go
@@ -1,1 +1,17 @@
 package resourcemanager
+
+import (
+	"net/http"
+	"sync"
+)
+
+// ResourceManager 资源管理器
+type ResourceManager struct {
+	mu               sync.RWMutex
+	applications     map[string]*Application
+	nodes            map[string]*Node
+	scheduler        Scheduler
+	appIDCounter     int32
+	clusterTimestamp int64
+	httpServer       *http.Server
+}
