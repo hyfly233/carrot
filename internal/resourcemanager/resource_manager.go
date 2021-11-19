0d308d3743af614a4355fb6d2fa2f89d443fe3d0
hyfly233 <hyfly233@outlook.com>
2021-11-19 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager.go
+++ internal/resourcemanager/resource_manager.go
@@ -1,8 +1,10 @@
 package resourcemanager
 
 import (
+	"carrot/internal/common"
 	"net/http"
 	"sync"
+	"time"
 )
 
 // ResourceManager 资源管理器
@@ -14,4 +16,20 @@ type ResourceManager struct {
 	appIDCounter     int32
 	clusterTimestamp int64
 	httpServer       *http.Server
+}
+
+// Application 应用程序
+type Application struct {
+	ID              common.ApplicationID          `json:"id"`
+	Name            string                        `json:"name"`
+	Type            string                        `json:"type"`
+	User            string                        `json:"user"`
+	Queue           string                        `json:"queue"`
+	State           string                        `json:"state"`
+	StartTime       time.Time                     `json:"start_time"`
+	FinishTime      time.Time                     `json:"finish_time,omitempty"`
+	Progress        float32                       `json:"progress"`
+	Attempts        []*ApplicationAttempt         `json:"attempts"`
+	AMContainerSpec common.ContainerLaunchContext `json:"am_container_spec"`
+	Resource        common.Resource               `json:"resource"`
 }
