9c9b95739c11bed80c8c051019b1686b03f84dc2
hyfly233 <hyfly233@outlook.com>
2021-11-20 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager.go
+++ internal/resourcemanager/resource_manager.go
@@ -33,3 +33,13 @@ type Application struct {
 	AMContainerSpec common.ContainerLaunchContext `json:"am_container_spec"`
 	Resource        common.Resource               `json:"resource"`
 }
+
+// ApplicationAttempt 应用程序尝试
+type ApplicationAttempt struct {
+	ID          common.ApplicationAttemptID `json:"id"`
+	State       string                      `json:"state"`
+	StartTime   time.Time                   `json:"start_time"`
+	FinishTime  time.Time                   `json:"finish_time,omitempty"`
+	AMContainer *common.Container           `json:"am_container,omitempty"`
+	TrackingURL string                      `json:"tracking_url"`
+}
