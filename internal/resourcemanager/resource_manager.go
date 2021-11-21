d9cfd071a5d33699c1976b663f8a3c7e90f7fa8b
hyfly233 <hyfly233@outlook.com>
2021-11-21 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager.go
+++ internal/resourcemanager/resource_manager.go
@@ -43,3 +43,16 @@ type ApplicationAttempt struct {
 	AMContainer *common.Container           `json:"am_container,omitempty"`
 	TrackingURL string                      `json:"tracking_url"`
 }
+
+// Node 节点
+type Node struct {
+	ID                common.NodeID                `json:"id"`
+	HTTPAddress       string                       `json:"http_address"`
+	RackName          string                       `json:"rack_name"`
+	TotalResource     common.Resource              `json:"total_resource"`
+	UsedResource      common.Resource              `json:"used_resource"`
+	AvailableResource common.Resource              `json:"available_resource"`
+	State             string                       `json:"state"`
+	LastHeartbeat     time.Time                    `json:"last_heartbeat"`
+	Containers        map[string]*common.Container `json:"containers"`
+}
