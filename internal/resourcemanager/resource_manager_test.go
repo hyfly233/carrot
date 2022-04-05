378737bd50f3f5d60df9410e9dfa524aeb961556
hyfly233 <hyfly233@outlook.com>
2022-04-05 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager_test.go
+++ internal/resourcemanager/resource_manager_test.go
@@ -1,6 +1,9 @@
 package resourcemanager
 
-import "testing"
+import (
+	"carrot/internal/common"
+	"testing"
+)
 
 func TestResourceManagerCreation(t *testing.T) {
 	rm := NewResourceManager()
@@ -18,5 +21,45 @@ func TestResourceManagerCreation(t *testing.T) {
 
 	if rm.nodes == nil {
 		t.Error("Nodes map not initialized")
+	}
+}
+
+func TestNodeRegistration(t *testing.T) {
+	rm := NewResourceManager()
+
+	nodeID := common.NodeID{
+		Host: "test-host",
+		Port: 8042,
+	}
+
+	resource := common.Resource{
+		Memory: 4096,
+		VCores: 4,
+	}
+
+	err := rm.RegisterNode(nodeID, resource, "http://test-host:8042")
+	if err != nil {
+		t.Fatalf("Failed to register node: %v", err)
+	}
+
+	nodes := rm.GetNodes()
+	if len(nodes) != 1 {
+		t.Errorf("Expected 1 node, got %d", len(nodes))
+	}
+
+	// 验证节点信息
+	for _, node := range nodes {
+		if node.NodeID.Host != "test-host" {
+			t.Errorf("Expected host 'test-host', got '%s'", node.NodeID.Host)
+		}
+		if node.NodeID.Port != 8042 {
+			t.Errorf("Expected port 8042, got %d", node.NodeID.Port)
+		}
+		if node.Capability.Memory != 4096 {
+			t.Errorf("Expected memory 4096, got %d", node.Capability.Memory)
+		}
+		if node.Capability.VCores != 4 {
+			t.Errorf("Expected vcores 4, got %d", node.Capability.VCores)
+		}
 	}
 }
