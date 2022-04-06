ea23111b019d54e3935918d51360729220904ebf
hyfly233 <hyfly233@outlook.com>
2022-04-06 12:00:00 +0800

    commit

--- internal/resourcemanager/resource_manager_test.go
+++ internal/resourcemanager/resource_manager_test.go
@@ -3,6 +3,7 @@ package resourcemanager
 import (
 	"carrot/internal/common"
 	"testing"
+	"time"
 )
 
 func TestResourceManagerCreation(t *testing.T) {
@@ -61,5 +62,42 @@ func TestNodeRegistration(t *testing.T) {
 		if node.Capability.VCores != 4 {
 			t.Errorf("Expected vcores 4, got %d", node.Capability.VCores)
 		}
+	}
+}
+
+func TestApplicationSubmission(t *testing.T) {
+	rm := NewResourceManager()
+
+	// 先注册一个节点
+	nodeID := common.NodeID{Host: "test-host", Port: 8042}
+	resource := common.Resource{Memory: 4096, VCores: 4}
+	rm.RegisterNode(nodeID, resource, "http://test-host:8042")
+
+	// 提交应用程序
+	ctx := common.ApplicationSubmissionContext{
+		ApplicationID:   common.ApplicationID{ClusterTimestamp: time.Now().Unix(), ID: 1},
+		ApplicationName: "test-app",
+		ApplicationType: "YARN",
+		Queue:           "default",
+		Priority:        1,
+		Resource:        common.Resource{Memory: 1024, VCores: 1},
+		AMContainerSpec: common.ContainerLaunchContext{
+			Commands: []string{"echo 'test'"},
+		},
+		MaxAppAttempts: 2,
+	}
+
+	appID, err := rm.SubmitApplication(ctx)
+	if err != nil {
+		t.Fatalf("Failed to submit application: %v", err)
+	}
+
+	if appID == nil {
+		t.Fatal("Application ID is nil")
+	}
+
+	apps := rm.GetApplications()
+	if len(apps) != 1 {
+		t.Errorf("Expected 1 application, got %d", len(apps))
 	}
 }
