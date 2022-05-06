cfd5487254945fcb5daf729c5c77335d97d7655e
hyfly233 <hyfly233@outlook.com>
2022-05-06 12:00:00 +0800

    commit

--- cmd/resourcemanager/main.go
+++ cmd/resourcemanager/main.go
@@ -2,22 +2,56 @@ package main
 
 import (
 	"flag"
-	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"carrot/internal/common"
 	"carrot/internal/resourcemanager"
+
+	"go.uber.org/zap"
 )
 
 func main() {
 	var (
-		port = flag.Int("port", 8088, "ResourceManager port")
+		port        = flag.Int("port", 8088, "ResourceManager port")
+		development = flag.Bool("dev", false, "Enable development mode")
 	)
 	flag.Parse()
 
-	log.Println("Starting YARN ResourceManager...")
+	// 初始化日志系统
+	if err := common.InitLogger(*development); err != nil {
+		panic(err)
+	}
+	defer common.Sync()
 
-	rm := resourcemanager.NewResourceManager()
+	logger := common.GetLogger()
+	logger.Info("Starting YARN ResourceManager",
+		zap.Int("port", *port),
+		zap.Bool("development", *development))
 
+	// 获取配置
+	config := common.GetDefaultConfig()
+	config.ResourceManager.Port = *port
+
+	// 创建ResourceManager
+	rm := resourcemanager.NewResourceManager(config)
+
+	// 优雅关闭处理
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		logger.Info("Received shutdown signal")
+		if err := rm.Stop(); err != nil {
+			logger.Error("Error stopping ResourceManager", zap.Error(err))
+		}
+		os.Exit(0)
+	}()
+
+	// 启动服务
 	if err := rm.Start(*port); err != nil {
-		log.Fatalf("Failed to start ResourceManager: %v", err)
+		logger.Fatal("Failed to start ResourceManager", zap.Error(err))
 	}
 }
