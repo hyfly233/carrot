3bd6c28615f59d2b413ee71d1a7f065a9919da39
hyfly233 <hyfly233@outlook.com>
2022-05-12 12:00:00 +0800

    commit

--- internal/resourcemanager/scheduler/scheduler.go
+++ internal/resourcemanager/scheduler/scheduler.go
@@ -1,30 +1,53 @@
 package scheduler
 
-import "carrot/internal/common"
+import (
+	"carrot/internal/common"
+	"time"
+)
 
 // Scheduler 调度器接口
 type Scheduler interface {
-	Schedule(app *ApplicationInfo) ([]*common.Container, error)
-	AllocateContainers(requests []common.ContainerRequest) ([]*common.Container, error)
+	// Schedule 调度应用程序，返回分配的容器列表
+	Schedule(appInfo *ApplicationInfo) ([]*ContainerAllocation, error)
+
+	// SetResourceManager 设置资源管理器引用
 	SetResourceManager(rm ResourceManagerInterface)
 }
 
-// ResourceManagerInterface 定义资源管理器接口，避免循环依赖
+// ResourceManagerInterface 资源管理器接口
 type ResourceManagerInterface interface {
 	GetNodesForScheduler() map[string]*NodeInfo
 	GetClusterTimestamp() int64
 }
 
-// NodeInfo 节点信息（简化版本，避免循环依赖）
+// ApplicationInfo 应用程序信息
+type ApplicationInfo struct {
+	ID         common.ApplicationID `json:"id"`
+	Resource   common.Resource      `json:"resource"`
+	SubmitTime time.Time            `json:"submit_time"`
+	Queue      string               `json:"queue"`
+	Priority   int32                `json:"priority"`
+}
+
+// ContainerAllocation 容器分配信息
+type ContainerAllocation struct {
+	ID       common.ContainerID `json:"id"`
+	NodeID   common.NodeID      `json:"node_id"`
+	Resource common.Resource    `json:"resource"`
+	Priority int32              `json:"priority"`
+}
+
+// NodeInfo 节点信息
 type NodeInfo struct {
 	ID                common.NodeID   `json:"id"`
 	State             string          `json:"state"`
 	AvailableResource common.Resource `json:"available_resource"`
 	UsedResource      common.Resource `json:"used_resource"`
+	LastHeartbeat     time.Time       `json:"last_heartbeat"`
 }
 
-// ApplicationInfo 应用程序信息（简化版本）
-type ApplicationInfo struct {
-	ID       common.ApplicationID `json:"id"`
-	Resource common.Resource      `json:"resource"`
+// HasSufficientResource 检查节点是否有足够的资源
+func (n *NodeInfo) HasSufficientResource(required common.Resource) bool {
+	return n.AvailableResource.Memory >= required.Memory &&
+		n.AvailableResource.VCores >= required.VCores
 }
