af599325dd56290f1ae1c88a1f3d7edaa1d14cf9
hyfly233 <hyfly233@outlook.com>
2022-06-13 12:00:00 +0800

    commit

--- internal/resourcemanager/recovery/recovery_manager.go
+++ internal/resourcemanager/recovery/recovery_manager.go
@@ -1,1 +1,485 @@
 package recovery
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"carrot/internal/common"
+
+	"go.uber.org/zap"
+)
+
+// RecoveryManager 恢复管理器
+type RecoveryManager struct {
+	mu                  sync.RWMutex
+	stateStore          StateStore
+	enabled             bool
+	recoveryInProgress  bool
+	checkpointInterval  time.Duration
+	maxRecoveryAttempts int
+	logger              *zap.Logger
+	ctx                 context.Context
+	cancel              context.CancelFunc
+
+	// 恢复状态
+	lastCheckpoint   time.Time
+	recoveryAttempts int
+
+	// 状态快照
+	currentSnapshot *ClusterSnapshot
+
+	// 事件通道
+	recoveryEvents chan *RecoveryEvent
+}
+
+// RecoveryManagerConfig 恢复管理器配置
+type RecoveryManagerConfig struct {
+	Enabled             bool                   `json:"enabled"`
+	CheckpointInterval  time.Duration          `json:"checkpoint_interval"`
+	MaxRecoveryAttempts int                    `json:"max_recovery_attempts"`
+	StateStoreType      string                 `json:"state_store_type"`
+	StateStoreConfig    map[string]interface{} `json:"state_store_config"`
+}
+
+// RecoveryEvent 恢复事件
+type RecoveryEvent struct {
+	Type      string                 `json:"type"`
+	Timestamp time.Time              `json:"timestamp"`
+	Data      map[string]interface{} `json:"data"`
+}
+
+// RecoveryEventType 恢复事件类型
+const (
+	RecoveryEventCheckpoint    = "CHECKPOINT_CREATED"
+	RecoveryEventStarted       = "RECOVERY_STARTED"
+	RecoveryEventCompleted     = "RECOVERY_COMPLETED"
+	RecoveryEventFailed        = "RECOVERY_FAILED"
+	RecoveryEventStateRestored = "STATE_RESTORED"
+)
+
+// ClusterSnapshot 集群快照
+type ClusterSnapshot struct {
+	Timestamp      time.Time              `json:"timestamp"`
+	Applications   []*ApplicationSnapshot `json:"applications"`
+	Nodes          []*NodeSnapshot        `json:"nodes"`
+	Resources      *ResourceSnapshot      `json:"resources"`
+	SchedulerState *SchedulerSnapshot     `json:"scheduler_state"`
+	Version        string                 `json:"version"`
+}
+
+// ApplicationSnapshot 应用程序快照
+type ApplicationSnapshot struct {
+	ID              common.ApplicationID          `json:"id"`
+	Name            string                        `json:"name"`
+	Type            string                        `json:"type"`
+	User            string                        `json:"user"`
+	Queue           string                        `json:"queue"`
+	State           string                        `json:"state"`
+	StartTime       time.Time                     `json:"start_time"`
+	Progress        float32                       `json:"progress"`
+	Attempts        []*ApplicationAttemptSnapshot `json:"attempts"`
+	AMContainerSpec common.ContainerLaunchContext `json:"am_container_spec"`
+	Resource        common.Resource               `json:"resource"`
+}
+
+// ApplicationAttemptSnapshot 应用程序尝试快照
+type ApplicationAttemptSnapshot struct {
+	ID          common.ApplicationAttemptID `json:"id"`
+	State       string                      `json:"state"`
+	StartTime   time.Time                   `json:"start_time"`
+	AMContainer *common.Container           `json:"am_container,omitempty"`
+	TrackingURL string                      `json:"tracking_url"`
+}
+
+// NodeSnapshot 节点快照
+type NodeSnapshot struct {
+	ID            common.NodeID       `json:"id"`
+	TotalResource common.Resource     `json:"total_resource"`
+	UsedResource  common.Resource     `json:"used_resource"`
+	State         string              `json:"state"`
+	HTTPAddress   string              `json:"http_address"`
+	LastHeartbeat time.Time           `json:"last_heartbeat"`
+	HealthReport  string              `json:"health_report"`
+	Containers    []*common.Container `json:"containers"`
+}
+
+// ResourceSnapshot 资源快照
+type ResourceSnapshot struct {
+	TotalMemory     int64 `json:"total_memory"`
+	TotalVCores     int32 `json:"total_vcores"`
+	UsedMemory      int64 `json:"used_memory"`
+	UsedVCores      int32 `json:"used_vcores"`
+	AvailableMemory int64 `json:"available_memory"`
+	AvailableVCores int32 `json:"available_vcores"`
+}
+
+// SchedulerSnapshot 调度器快照
+type SchedulerSnapshot struct {
+	Type          string                 `json:"type"`
+	Configuration map[string]interface{} `json:"configuration"`
+	QueueStates   map[string]interface{} `json:"queue_states"`
+}
+
+// RecoveryStatus 恢复状态
+type RecoveryStatus struct {
+	Enabled             bool      `json:"enabled"`
+	RecoveryInProgress  bool      `json:"recovery_in_progress"`
+	LastCheckpoint      time.Time `json:"last_checkpoint"`
+	RecoveryAttempts    int       `json:"recovery_attempts"`
+	MaxRecoveryAttempts int       `json:"max_recovery_attempts"`
+	LastRecoveryTime    time.Time `json:"last_recovery_time"`
+	LastRecoveryResult  string    `json:"last_recovery_result"`
+}
+
+// NewRecoveryManager 创建新的恢复管理器
+func NewRecoveryManager(config *RecoveryManagerConfig) (*RecoveryManager, error) {
+	if config == nil {
+		config = &RecoveryManagerConfig{
+			Enabled:             true,
+			CheckpointInterval:  5 * time.Minute,
+			MaxRecoveryAttempts: 3,
+			StateStoreType:      "memory",
+			StateStoreConfig:    make(map[string]interface{}),
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// 创建状态存储
+	stateStore, err := CreateStateStore(config.StateStoreType, config.StateStoreConfig)
+	if err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to create state store: %w", err)
+	}
+
+	rm := &RecoveryManager{
+		stateStore:          stateStore,
+		enabled:             config.Enabled,
+		checkpointInterval:  config.CheckpointInterval,
+		maxRecoveryAttempts: config.MaxRecoveryAttempts,
+		logger:              common.ComponentLogger("recovery-manager"),
+		ctx:                 ctx,
+		cancel:              cancel,
+		recoveryEvents:      make(chan *RecoveryEvent, 1000),
+	}
+
+	if rm.enabled {
+		// 启动后台任务
+		go rm.checkpointWorker()
+		go rm.eventProcessor()
+	}
+
+	return rm, nil
+}
+
+// SaveCheckpoint 保存检查点
+func (rm *RecoveryManager) SaveCheckpoint(snapshot *ClusterSnapshot) error {
+	if !rm.enabled {
+		return nil
+	}
+
+	rm.mu.Lock()
+	defer rm.mu.Unlock()
+
+	snapshot.Timestamp = time.Now()
+	snapshot.Version = "1.0.0"
+
+	if err := rm.stateStore.SaveSnapshot(snapshot); err != nil {
+		rm.logger.Error("Failed to save checkpoint", zap.Error(err))
+		return err
+	}
+
+	rm.lastCheckpoint = snapshot.Timestamp
+	rm.currentSnapshot = snapshot
+
+	rm.logger.Info("Checkpoint saved",
+		zap.Time("timestamp", snapshot.Timestamp),
+		zap.Int("applications", len(snapshot.Applications)),
+		zap.Int("nodes", len(snapshot.Nodes)))
+
+	// 发送事件
+	rm.sendEvent(&RecoveryEvent{
+		Type:      RecoveryEventCheckpoint,
+		Timestamp: time.Now(),
+		Data: map[string]interface{}{
+			"applications_count": len(snapshot.Applications),
+			"nodes_count":        len(snapshot.Nodes),
+			"timestamp":          snapshot.Timestamp,
+		},
+	})
+
+	return nil
+}
+
+// StartRecovery 开始恢复过程
+func (rm *RecoveryManager) StartRecovery() error {
+	if !rm.enabled {
+		return fmt.Errorf("recovery is disabled")
+	}
+
+	rm.mu.Lock()
+	defer rm.mu.Unlock()
+
+	if rm.recoveryInProgress {
+		return fmt.Errorf("recovery already in progress")
+	}
+
+	if rm.recoveryAttempts >= rm.maxRecoveryAttempts {
+		return fmt.Errorf("maximum recovery attempts (%d) exceeded", rm.maxRecoveryAttempts)
+	}
+
+	rm.recoveryInProgress = true
+	rm.recoveryAttempts++
+
+	rm.logger.Info("Starting recovery process",
+		zap.Int("attempt", rm.recoveryAttempts),
+		zap.Int("max_attempts", rm.maxRecoveryAttempts))
+
+	// 发送事件
+	rm.sendEvent(&RecoveryEvent{
+		Type:      RecoveryEventStarted,
+		Timestamp: time.Now(),
+		Data: map[string]interface{}{
+			"attempt":      rm.recoveryAttempts,
+			"max_attempts": rm.maxRecoveryAttempts,
+		},
+	})
+
+	// 在后台执行恢复
+	go rm.performRecovery()
+
+	return nil
+}
+
+// performRecovery 执行恢复过程
+func (rm *RecoveryManager) performRecovery() {
+	defer func() {
+		rm.mu.Lock()
+		rm.recoveryInProgress = false
+		rm.mu.Unlock()
+	}()
+
+	// 加载最新快照
+	snapshot, err := rm.stateStore.LoadLatestSnapshot()
+	if err != nil {
+		rm.logger.Error("Failed to load snapshot for recovery", zap.Error(err))
+		rm.sendRecoveryFailedEvent(err)
+		return
+	}
+
+	if snapshot == nil {
+		rm.logger.Warn("No snapshot found for recovery")
+		rm.sendRecoveryFailedEvent(fmt.Errorf("no snapshot available"))
+		return
+	}
+
+	rm.logger.Info("Loaded snapshot for recovery",
+		zap.Time("snapshot_time", snapshot.Timestamp),
+		zap.Int("applications", len(snapshot.Applications)),
+		zap.Int("nodes", len(snapshot.Nodes)))
+
+	// 执行状态恢复
+	if err := rm.restoreClusterState(snapshot); err != nil {
+		rm.logger.Error("Failed to restore cluster state", zap.Error(err))
+		rm.sendRecoveryFailedEvent(err)
+		return
+	}
+
+	rm.logger.Info("Recovery completed successfully")
+
+	// 发送恢复完成事件
+	rm.sendEvent(&RecoveryEvent{
+		Type:      RecoveryEventCompleted,
+		Timestamp: time.Now(),
+		Data: map[string]interface{}{
+			"snapshot_timestamp":    snapshot.Timestamp,
+			"applications_restored": len(snapshot.Applications),
+			"nodes_restored":        len(snapshot.Nodes),
+		},
+	})
+
+	// 重置恢复尝试计数
+	rm.mu.Lock()
+	rm.recoveryAttempts = 0
+	rm.mu.Unlock()
+}
+
+// restoreClusterState 恢复集群状态
+func (rm *RecoveryManager) restoreClusterState(snapshot *ClusterSnapshot) error {
+	// 这里实现具体的状态恢复逻辑
+	// 在实际实现中，需要调用 ResourceManager 的相应方法来恢复状态
+
+	rm.logger.Info("Restoring cluster state",
+		zap.Time("snapshot_time", snapshot.Timestamp))
+
+	// 恢复应用程序状态
+	for _, appSnapshot := range snapshot.Applications {
+		rm.logger.Debug("Restoring application",
+			zap.String("app_id", fmt.Sprintf("%d_%d",
+				appSnapshot.ID.ClusterTimestamp, appSnapshot.ID.ID)),
+			zap.String("state", appSnapshot.State))
+
+		// TODO: 调用 ApplicationManager 恢复应用程序状态
+	}
+
+	// 恢复节点状态
+	for _, nodeSnapshot := range snapshot.Nodes {
+		rm.logger.Debug("Restoring node",
+			zap.String("node_id", fmt.Sprintf("%s:%d",
+				nodeSnapshot.ID.Host, nodeSnapshot.ID.Port)),
+			zap.String("state", nodeSnapshot.State))
+
+		// TODO: 调用 NodeTracker 恢复节点状态
+	}
+
+	// 恢复调度器状态
+	if snapshot.SchedulerState != nil {
+		rm.logger.Debug("Restoring scheduler state",
+			zap.String("type", snapshot.SchedulerState.Type))
+
+		// TODO: 调用 Scheduler 恢复状态
+	}
+
+	rm.sendEvent(&RecoveryEvent{
+		Type:      RecoveryEventStateRestored,
+		Timestamp: time.Now(),
+		Data: map[string]interface{}{
+			"applications_count": len(snapshot.Applications),
+			"nodes_count":        len(snapshot.Nodes),
+		},
+	})
+
+	return nil
+}
+
+// GetRecoveryStatus 获取恢复状态
+func (rm *RecoveryManager) GetRecoveryStatus() *RecoveryStatus {
+	rm.mu.RLock()
+	defer rm.mu.RUnlock()
+
+	return &RecoveryStatus{
+		Enabled:             rm.enabled,
+		RecoveryInProgress:  rm.recoveryInProgress,
+		LastCheckpoint:      rm.lastCheckpoint,
+		RecoveryAttempts:    rm.recoveryAttempts,
+		MaxRecoveryAttempts: rm.maxRecoveryAttempts,
+	}
+}
+
+// GetLatestSnapshot 获取最新快照
+func (rm *RecoveryManager) GetLatestSnapshot() (*ClusterSnapshot, error) {
+	if !rm.enabled {
+		return nil, fmt.Errorf("recovery is disabled")
+	}
+
+	return rm.stateStore.LoadLatestSnapshot()
+}
+
+// ListSnapshots 列出所有快照
+func (rm *RecoveryManager) ListSnapshots(limit int) ([]*SnapshotMetadata, error) {
+	if !rm.enabled {
+		return nil, fmt.Errorf("recovery is disabled")
+	}
+
+	return rm.stateStore.ListSnapshots(limit)
+}
+
+// DeleteOldSnapshots 删除旧快照
+func (rm *RecoveryManager) DeleteOldSnapshots(olderThan time.Time) error {
+	if !rm.enabled {
+		return fmt.Errorf("recovery is disabled")
+	}
+
+	return rm.stateStore.DeleteOldSnapshots(olderThan)
+}
+
+// checkpointWorker 检查点工作线程
+func (rm *RecoveryManager) checkpointWorker() {
+	ticker := time.NewTicker(rm.checkpointInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// 这里应该调用外部函数来获取当前集群状态
+			// 并创建检查点，但为了简化，这里只记录日志
+			rm.logger.Debug("Checkpoint interval reached")
+
+		case <-rm.ctx.Done():
+			return
+		}
+	}
+}
+
+// eventProcessor 事件处理器
+func (rm *RecoveryManager) eventProcessor() {
+	for {
+		select {
+		case event, ok := <-rm.recoveryEvents:
+			if !ok {
+				// 通道已关闭
+				return
+			}
+			rm.logger.Debug("Processing recovery event",
+				zap.String("event_type", event.Type))
+			// 这里可以添加事件处理逻辑
+
+		case <-rm.ctx.Done():
+			return
+		}
+	}
+}
+
+// sendEvent 发送事件
+func (rm *RecoveryManager) sendEvent(event *RecoveryEvent) {
+	select {
+	case rm.recoveryEvents <- event:
+	default:
+		rm.logger.Warn("Recovery event channel full, dropping event",
+			zap.String("event_type", event.Type))
+	}
+}
+
+// sendRecoveryFailedEvent 发送恢复失败事件
+func (rm *RecoveryManager) sendRecoveryFailedEvent(err error) {
+	rm.sendEvent(&RecoveryEvent{
+		Type:      RecoveryEventFailed,
+		Timestamp: time.Now(),
+		Data: map[string]interface{}{
+			"error":   err.Error(),
+			"attempt": rm.recoveryAttempts,
+		},
+	})
+}
+
+// IsEnabled 检查恢复是否启用
+func (rm *RecoveryManager) IsEnabled() bool {
+	rm.mu.RLock()
+	defer rm.mu.RUnlock()
+	return rm.enabled
+}
+
+// SetEnabled 设置恢复启用状态
+func (rm *RecoveryManager) SetEnabled(enabled bool) {
+	rm.mu.Lock()
+	defer rm.mu.Unlock()
+
+	rm.enabled = enabled
+	rm.logger.Info("Recovery enabled status changed", zap.Bool("enabled", enabled))
+}
+
+// Stop 停止恢复管理器
+func (rm *RecoveryManager) Stop() error {
+	rm.logger.Info("Stopping recovery manager")
+
+	rm.cancel()
+	close(rm.recoveryEvents)
+
+	if rm.stateStore != nil {
+		return rm.stateStore.Close()
+	}
+
+	return nil
+}
